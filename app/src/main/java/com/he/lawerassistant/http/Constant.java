60dfea51754a36d721c6e2fe00f9aec42c557f72
刘贺 <liuhe@txcap.com>
2018-11-04 13:15:03 +0800

    上传apk

--- app/src/main/java/com/he/lawerassistant/http/Constant.java
+++ app/src/main/java/com/he/lawerassistant/http/Constant.java
@@ -2,6 +2,8 @@ package com.he.lawerassistant.http;
 
 public class Constant {
     public static String RETROFIT_CACHE_DIR = "retrofit_cache";
+    public static String ISNIGHT = "is_night";
 
-    public static String BASE_URL = "http://192.168.20.239:5000";
+//    public static String BASE_URL = "http://192.168.20.239:5000";
+    public static String BASE_URL = "http://18.220.87.46:8989";
 }
---
+++ app/src/main/java/com/he/lawerassistant/ui/activity/WebActivity.java
@@ -0,0 +1,122 @@
+package com.he.lawerassistant.ui.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.he.lawerassistant.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WebActivity extends BaseActivity {
+
+    @BindView(R.id.web_view)
+    WebView webView;
+
+    String url = "";
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        ButterKnife.bind(this);
+        url = getIntent().getStringExtra("url");
+        toolbarTitle.setText(parserUrl(url));
+
+        this.setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url != null && url.endsWith(".html")) {
+                    Intent intent = new Intent(WebActivity.this, WebActivity.class);
+                    intent.putExtra("url", url);
+                    Log.e("xxxx","shouldOverrideUrlLoading"+url);
+                    startActivity(intent);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (url != null && url.endsWith(".html")) {
+                    Intent intent = new Intent(WebActivity.this, WebActivity.class);
+                    intent.putExtra("url", url);
+                    Log.e("xxxx","shouldOverrideUrlLoading"+url);
+                    startActivity(intent);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+
+        webView.loadUrl(url);
+
+        /*webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+                        webView.goBack();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });*/
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String parserUrl(String url) {
+        String title = "";
+        List<String> strings = Arrays.asList(url.split("/"));
+        String t = strings.get(strings.size() - 1);
+        List<String> ts  = Arrays.asList(t.split("\\."));
+        if(ts.size() > 0){
+            title = ts.get(0);
+        }
+
+        return Uri.decode(title);
+
+    }
+
+
+}
